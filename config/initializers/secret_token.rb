9abff587c603c06f7a004413aaeb1abf8c7e0bde
Emanuele DelBono <emanuele@codiceplastico.com>
2014-01-31 17:48:16 +0100

    configured rvm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Revents::Application.config.secret_key_base = '114bd9b81df9cd5df5c7768d5bae60af8c020437551061771e008c70538af0c5800b226b8dfb11405e39d3628d448bbe2d5e3176d3a1d7e33f5da0e81d4ea80b'
