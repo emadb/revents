ec68db021b560a14adf05d35351a1484b08c0dce
Emanuele DelBono <emanuele@codiceplastico.com>
2014-02-14 19:32:19 +0100

    added additiona infos

--- server/app/denormalizers/basket_denormalizer.rb
+++ server/app/denormalizers/basket_denormalizer.rb
@@ -6,14 +6,18 @@ class BasketDenormalizer
   end
 
   def consume(data)
-    # TODO: add article information (description, price)
     db = Sequel.sqlite(AppSettings.sql_connection)
-    
+    article = db[:products_view].where(id: data['article_id']).first
     basket = db[:basket_view].where('basket_id = ? AND article_id = ?', data['basket_id'], data['article_id']).first
     if basket.nil?
-      db[:basket_view].insert(basket_id: data['basket_id'], article_id: data['article_id'], article_description: 'test1', article_price: 10, quantity: 1)
+      db[:basket_view].insert(
+        basket_id: data['basket_id'], 
+        article_id: data['article_id'], 
+        article_description: article[:description], 
+        article_price: article[:price], 
+        quantity: 1)
     else
-      db[:basket_view].where(:id => basket[:id]).update(:quantity => (basket[:quantity] + 1))        
+      db[:basket_view].where(id: basket[:id]).update(quantity: (basket[:quantity] + 1))        
     end  
   end
 end
